3af17dcf9be803921273662fe687bad6bde1ceec
Gaby <gabybabuchi@gmail.com>
2021-09-27 12:03:16 -0400

    Modify contract to mint NFTs.
    Use openzeppelin features to mint
    nfts to user's address.

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,11 +2,35 @@
 
 pragma solidity ^0.8.0;
 
+//import OpenZeppelin Contracts
+import "@openzeppelin/contracts/token/ECR721/extensions/ECR721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 //a hardhat goodie to debug smart contract by console logs
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor(){
+contract MyEpicNFT is ERC721URIStorage{
+    //openzeppelin feature to keep track of tokens ids
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    //we need to pass the name of our NFTs and it's symbol
+    constructor() ERC721("SquareNFT", "Square") {
         console.log("Super dope NFT contract");
+    }
+
+    //a function that allows users to get an nft
+    function makeAnEpicNFT() public {
+        //Get the current tokenId, this starts at 0
+        uint256 newItemId = _tokenIds.current();
+
+        //Actually mint the NFT to the sender using msg.sender
+        _safeMint(msg.sender, newItemId);
+
+        //Set the NFTs data that makes it valuable
+        //Later this call will be modified to follow the standards(a json with specific properties)
+        _setTokenURI(newItemId, "blah");
+
+        //Increment the counter for when the next NFT is minted
+        _tokenIds.increment();
     }
 }
